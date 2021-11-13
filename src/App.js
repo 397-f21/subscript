5efd18eb294c1aea6e30b206d14c30a57c5eb155
Zachary Deng <zacdeng@Zacharys-MacBook-Pro.local>
2021-11-13 17:43:51 -0600

    Title & logo

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from "react";
+import "./App.css";
 
-function App() {
+const Title = {
+  title: "subscript",
+  subTitle: "Never pay for an unwanted subscription again! " +
+      "subscript instantly finds and tracks your subscriptions. " +
+      "Your concierge is there when you need them to cancel services so you don’t have to.",
+};
+
+export const Banner = ({ title, subTitle }) => {
+  return (<div className="App-Title">
+    <img
+        data-cy="logo"
+        src="https://i.loli.net/2021/11/14/2crIksEnlbBHjxz.png"
+        alt="AppLogo"
+        style={{ width: 50, height: 45 }}
+    />
+    <h1 data-cy="title">{title}</h1>
+    <i data-cy="subtitle">
+      <p>{subTitle}</p>
+    </i>
+  </div>)
+};
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="container">
+        <Banner title={Title.title} subTitle={Title.subTitle} />
+        <div className="body-container">
+
+        </div>
+      </div>
   );
-}
+};
 
 export default App;
