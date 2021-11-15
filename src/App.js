2dbf5c43847bfa2ff2623f8dc6b2e97a2b169928
zacdeng <zacdeng0720@gmail.com>
2021-11-15 15:08:17 -0600

    change logo & add fix bottom banner

--- src/App.js
+++ src/App.js
@@ -12,34 +12,46 @@ const Title = {
 };
 
 export const Banner = ({ title, subtitle, descriptionLine1, descriptionLine2 }) => {
-  return (<div className="Banner" data-cy="Banner">
-      <img
-          data-cy="logo"
-          src="https://i.loli.net/2021/11/14/2crIksEnlbBHjxz.png"
-          alt="AppLogo"
-          style={{ width: 60, height: 60}}
-      />
-      <div className="App-Title"> {title} </div>
-      <div className="App-Subtitle"> <p>{subtitle}</p> </div>
-      <div className="App-Description">
-          <i>{descriptionLine1}</i>
-          <i><p>{descriptionLine2}</p></i>
-      </div>
+  return (
+      <div className="Banner" data-cy="Banner">
+          <img
+              data-cy="logo"
+              src="https://i.loli.net/2021/11/16/CA6pPW1X8zIeGVL.png"
+              alt="AppLogo"
+              style={{ width: 60, height: 60}}
+          />
+          <div className="App-Title"> {title} </div>
+          <div className="App-Subtitle"> <p>{subtitle}</p> </div>
+          <div className="App-Description">
+              <i>{descriptionLine1}</i>
+              <i><p>{descriptionLine2}</p></i>
+          </div>
     </div>)
 };
+
+const Bottom = () => {
+    return (
+        <div>
+            <p>This is the bottom banner for test.</p>
+        </div>
+    )
+}
 
 const App = () => {
   return (
       <div className="container">
-        <Banner title={Title.title}
-                subtitle={Title.subtitle}
-                descriptionLine1={Title.descriptionLine1}
-                descriptionLine2={Title.descriptionLine2}
-        />
-        <div className="body-container">
-            <LeftCard className="leftcard"/>
-            <RightCard className="rightcard"/>
-        </div>
+          <Banner title={Title.title}
+                  subtitle={Title.subtitle}
+                  descriptionLine1={Title.descriptionLine1}
+                  descriptionLine2={Title.descriptionLine2}
+          />
+          <div className="body-container">
+              <LeftCard className="leftcard"/>
+              <RightCard className="rightcard"/>
+          </div>
+          <div className="bottom">
+              <Bottom />
+          </div>
       </div>
   );
 };
