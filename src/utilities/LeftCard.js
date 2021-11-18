855ed737fde26f71365168dd9018da62a3901b7b
Kathryne Tao <kathryne@Kathrynes-MacBook-Pro-20.local>
2021-11-17 23:17:44 -0600

    styling

--- src/utilities/LeftCard.js
+++ src/utilities/LeftCard.js
@@ -135,42 +135,42 @@ const LeftCardStatic = () => {
       <div className="descriptions">
         <div>{Descriptions.description}</div>
       </div>
-
-      {/* <div className="demoList">
-                <div className="demoItem">
-                    <spanc className="column1">
-                        <img
-                            src="https://i.loli.net/2021/11/15/LZAwp2WV38J9zhu.png"
-                            alt="Netflix"
-                            style={{width:30, height:30}}/>
-                    </spanc>
-                    <span className="column2">Netfix</span>
-                    <span className="column3">$17.99</span>
-                </div>
-                <div className="demoItem">
-                    <span className="column1">
-                        <img
-                            src="https://i.loli.net/2021/11/15/d8Ls5IxvwW7POug.png"
-                            alt="Adobe"
-                            style={{width:30, height:30}}/>
-                    </span>
-                    <span className="column2">Adobe</span>
-                    <span className="column3">$19.99</span>
-                </div>
-                <div className="demoItem">
-                    <span className="column1">
-                        <img
-                            src="https://i.loli.net/2021/11/15/zfo6EG4AdhimQ3n.png"
-                            alt="Spotify"
-                            style={{width:30, height:30}}/>
-                    </span>
-                    <span className="column2">Spotify</span>
-                    <span className="column3">$4.99</span>
-                </div>
-            </div> */}
     </div>
   );
 };
+
+/* <div className="demoList">
+          <div className="demoItem">
+              <spanc className="column1">
+                  <img
+                      src="https://i.loli.net/2021/11/15/LZAwp2WV38J9zhu.png"
+                      alt="Netflix"
+                      style={{width:30, height:30}}/>
+              </spanc>
+              <span className="column2">Netfix</span>
+              <span className="column3">$17.99</span>
+          </div>
+          <div className="demoItem">
+              <span className="column1">
+                  <img
+                      src="https://i.loli.net/2021/11/15/d8Ls5IxvwW7POug.png"
+                      alt="Adobe"
+                      style={{width:30, height:30}}/>
+              </span>
+              <span className="column2">Adobe</span>
+              <span className="column3">$19.99</span>
+          </div>
+          <div className="demoItem">
+              <span className="column1">
+                  <img
+                      src="https://i.loli.net/2021/11/15/zfo6EG4AdhimQ3n.png"
+                      alt="Spotify"
+                      style={{width:30, height:30}}/>
+              </span>
+              <span className="column2">Spotify</span>
+              <span className="column3">$4.99</span>
+          </div>
+      </div> */
 
 export const LeftCard = ({ subscriptions, setSubscriptions }) => {
   const [user] = useUserState();
